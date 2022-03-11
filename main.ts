13662adacf4c1744bf9b03763634740c27d73e3f
naca-nyan <naca.nyan@gmail.com>
2022-03-11 11:16:40 +0900

    Improve logging

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Server } from "ws";
+import { RawData, Server } from "ws";
 
 const config = {
   port: 5000,
@@ -6,15 +6,35 @@ const config = {
 
 const server = new Server(config);
 
-server.on("connection", (ws) => {
+const log = (obj: object) => {
   const log = {
     timestamp: new Date().toISOString(),
-    message: "new client connected",
+    ...obj,
   };
   console.log(JSON.stringify(log));
-  ws.on("message", (message) => {
+};
+
+function getUser(url?: string): string {
+  const unknown = "unknown user";
+  if (url === undefined) return unknown;
+  const base = "https://example.com";
+  const u = new URL(url, base);
+  const user = u.searchParams.get("user");
+  return user ?? unknown;
+}
+
+server.on("connection", (ws, request) => {
+  const user = getUser(request.url);
+  log({ event: "new connection", user });
+  ws.onmessage = (message) => {
+    const body = message.data;
+    console.log(body);
+    log({ event: "message", user, body });
     server.clients.forEach((client) => {
-      client.send(message.toString());
+      client.send(body);
     });
-  });
+  };
+  ws.onclose = () => {
+    log({ event: "closed", user });
+  };
 });
