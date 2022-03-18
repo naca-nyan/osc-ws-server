13f51a8f23b6c66e0573b4b317216c7607952b10
naca-nyan <naca.nyan@gmail.com>
2022-03-19 01:38:30 +0900

    Add id

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -32,11 +47,27 @@ function getParams(url) {
     res.room = (_b = params.get("room")) !== null && _b !== void 0 ? _b : res.room;
     return res;
 }
+var globalId = 0;
+function getId() {
+    return globalId++;
+}
+var WebSocket = /** @class */ (function (_super) {
+    __extends(WebSocket, _super);
+    function WebSocket() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return WebSocket;
+}(ws_1.WebSocket));
 server.on("connection", function (ws, request) {
     var _a = getParams(request.url), user = _a.user, room = _a.room;
-    var event = { event: "connection", user: user, room: room };
+    var id = getId();
+    ws.id = id;
+    var event = { event: "connection", user: user, room: room, id: id };
+    ws.send(JSON.stringify({ event: "id", id: id, user: user, room: room }));
     log(event);
     server.clients.forEach(function (client) {
+        if (client.id === id)
+            return;
         var body = JSON.stringify(event);
         client.send(body);
     });
@@ -44,11 +75,22 @@ server.on("connection", function (ws, request) {
         var body = JSON.parse(message.data.toString());
         var event = { event: "message", user: user, room: room, body: body };
         log(event);
+        var to = body.to;
         server.clients.forEach(function (client) {
-            client.send(JSON.stringify(body));
+            if (client.id === id)
+                return;
+            if (to !== undefined && client.id !== to)
+                return;
+            client.send(JSON.stringify(event));
         });
     };
     ws.onclose = function () {
-        log({ event: "closed", user: user, room: room });
+        var event = { event: "closed", user: user, room: room, id: id };
+        server.clients.forEach(function (client) {
+            if (client.id === id)
+                return;
+            client.send(JSON.stringify(event));
+        });
+        log(event);
     };
 });
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Server } from "ws";
+import { Server, WebSocket as WebSocketBase } from "ws";
 
 const config = {
   port: 5000,
@@ -26,11 +26,24 @@ function getParams(url?: string) {
   return res;
 }
 
-server.on("connection", (ws, request) => {
+let globalId = 0;
+function getId() {
+  return globalId++;
+}
+
+class WebSocket extends WebSocketBase {
+  id?: number;
+}
+
+server.on("connection", (ws: WebSocket, request) => {
   const { user, room } = getParams(request.url);
-  const event = { event: "connection", user, room };
+  const id = getId();
+  ws.id = id;
+  const event = { event: "connection", user, room, id };
+  ws.send(JSON.stringify({ event: "id", id, user, room }));
   log(event);
-  server.clients.forEach((client) => {
+  server.clients.forEach((client: WebSocket) => {
+    if (client.id === id) return;
     const body = JSON.stringify(event);
     client.send(body);
   });
@@ -38,11 +51,19 @@ server.on("connection", (ws, request) => {
     const body = JSON.parse(message.data.toString());
     const event = { event: "message", user, room, body };
     log(event);
-    server.clients.forEach((client) => {
-      client.send(JSON.stringify(body));
+    const to = body.to as number | undefined;
+    server.clients.forEach((client: WebSocket) => {
+      if (client.id === id) return;
+      if (to !== undefined && client.id !== to) return;
+      client.send(JSON.stringify(event));
     });
   };
   ws.onclose = () => {
-    log({ event: "closed", user, room });
+    const event = { event: "closed", user, room, id };
+    server.clients.forEach((client: WebSocket) => {
+      if (client.id === id) return;
+      client.send(JSON.stringify(event));
+    });
+    log(event);
   };
 });
